2eea2e110fc5cb8f682ffd44a0bb7c65e2786101
liuxiu1 <1039544668@qq.com>
2021-06-01 15:26:04 +0800

    添加icon

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-16 18:52:34
+ * @LastEditTime: 2021-06-01 15:25:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /cupshe-element/packages/index.js
+ */
 import Button from './Button'
-
+import Icon from 'cupshe-icon'
 const components = [Button]
+Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2489304_9tjpddaxw0k.js'
+})
 const install = function(Vue) {
+  Vue.use(Icon)
   if (install.installed) return
   components.map(component => {
     Vue.component(component.name, component)
