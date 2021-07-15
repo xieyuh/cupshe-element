a758d2e8558d5b5aca6ec12e2c42ee077b671a74
xieyuheng <yuheng.xie@kapeixi.com>
2021-07-15 15:52:31 +0800

    feat(Popup): new popup component

---
+++ src/popup/demo/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <demo-block card title="基础用法">
+    <c-cell title="展示弹出层" is-link @click="showBasic = true" />
+    <c-popup v-model:show="showBasic" :style="{ padding: '30px 50px' }">
+      内容
+    </c-popup>
+  </demo-block>
+  <demo-block card title="弹出位置">
+    <c-cell title="顶部弹出" is-link @click="showTop = true" />
+    <c-cell title="底部弹出" is-link @click="showBottom = true" />
+    <c-cell title="左侧弹出" is-link @click="showLeft = true" />
+    <c-cell title="右侧弹出" is-link @click="showRight = true" />
+
+    <c-popup v-model:show="showTop" position="top" :style="{ height: '30%' }" />
+    <c-popup
+      v-model:show="showBottom"
+      position="bottom"
+      :style="{ height: '30%' }"
+    />
+    <c-popup
+      v-model:show="showLeft"
+      position="left"
+      :style="{ width: '30%', height: '100%' }"
+    />
+    <c-popup
+      v-model:show="showRight"
+      position="right"
+      :style="{ width: '30%', height: '100%' }"
+    />
+  </demo-block>
+
+  <demo-block card title="关闭图标">
+    <c-cell title="关闭图标" is-link @click="showCloseIcon = true" />
+    <c-cell title="自定义图标" is-link @click="showCustomCloseIcon = true" />
+    <c-cell title="图标位置" is-link @click="showCustomIconPosition = true" />
+
+    <c-popup
+      v-model:show="showCloseIcon"
+      closeable
+      position="bottom"
+      :style="{ height: '30%' }"
+    />
+    <c-popup
+      v-model:show="showCustomCloseIcon"
+      closeable
+      close-icon="tick"
+      position="bottom"
+      :style="{ height: '30%' }"
+    />
+    <c-popup
+      v-model:show="showCustomIconPosition"
+      closeable
+      close-icon-position="top-left"
+      position="bottom"
+      :style="{ height: '30%' }"
+    />
+  </demo-block>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      showBasic: false,
+      showTop: false,
+      showBottom: false,
+      showLeft: false,
+      showRight: false,
+      showCloseIcon: false,
+      showRoundCorner: false,
+      showGetContainer: false,
+      showCustomCloseIcon: false,
+      showCustomIconPosition: false,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="less">
+@import '../../style/var';
+
+.demo-popup {
+  .c-row {
+    margin-bottom: @padding-md;
+  }
+
+  .c-button {
+    margin-left: @padding-md;
+  }
+}
+</style>
---
+++ src/popup/index.vue
@@ -0,0 +1,234 @@
+<template>
+  <c-overlay
+    v-if="overlay"
+    :show="show"
+    :class="overlayClass"
+    :z-index="zIndex"
+    :duration="duration"
+    :custom-style="overlayStyle"
+    @click="onClickOverlay"
+  >
+    <slot name="overlay-content" />
+  </c-overlay>
+  <transition
+    :name="transition || name"
+    :appear="transitionAppear"
+    @after-enter="onOpened"
+    @after-leave="onClosed"
+  >
+    <div
+      v-show="show"
+      ref="popupRef"
+      :style="style"
+      :class="
+        bem({
+          [position]: position,
+        })
+      "
+      @click="onClick"
+      v-bind="attrs"
+    >
+      <slot />
+      <c-icon
+        v-if="closeable"
+        role="button"
+        tabindex="0"
+        :name="closeIcon"
+        :class="bem('close-icon', closeIconPosition)"
+        @click="onClickCloseIcon"
+      />
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  CSSProperties,
+  onActivated,
+  onDeactivated,
+  onMounted,
+  PropType,
+  provide,
+  ref,
+  watch,
+} from 'vue';
+import { createNamespace, extend, isDef } from '../utils';
+import { popupSharedProps } from './shared';
+import {
+  useEventListener,
+  useExpose,
+  useLockScroll,
+  POPUP_TOGGLE_KEY,
+} from '../composables';
+
+const [name, bem] = createNamespace('popup');
+
+export type PopupPosition = 'top' | 'left' | 'bottom' | 'right' | 'center' | '';
+
+export type PopupCloseIconPosition =
+  | 'top-left'
+  | 'top-right'
+  | 'bottom-left'
+  | 'bottom-right';
+
+let globalZIndex = 2000;
+
+export default {
+  name,
+
+  inheritAttrs: false,
+
+  props: extend({}, popupSharedProps, {
+    closeable: Boolean,
+    transition: String,
+    closeOnPopstate: Boolean,
+    position: {
+      type: String as PropType<PopupPosition>,
+      default: 'center',
+    },
+    closeIcon: {
+      type: String,
+      default: 'close',
+    },
+    closeIconPosition: {
+      type: String as PropType<PopupCloseIconPosition>,
+      default: 'top-right',
+    },
+  }),
+
+  emits: [
+    'open',
+    'close',
+    'click',
+    'opened',
+    'closed',
+    'update:show',
+    'click-overlay',
+    'click-close-icon',
+  ],
+
+  setup(props, { emit, attrs }) {
+    let opened: boolean;
+    let shouldReopen: boolean;
+    const zIndex = ref<number>();
+    const popupRef = ref<HTMLElement>();
+
+    const style = computed(() => {
+      const s: CSSProperties = {
+        zIndex: zIndex.value,
+      };
+
+      if (isDef(props.duration)) {
+        const key =
+          props.position === 'center'
+            ? 'animationDuration'
+            : 'transitionDuration';
+        s[key] = `${props.duration}s`;
+      }
+
+      return s;
+    });
+
+    const open = () => {
+      if (!opened) {
+        if (props.zIndex !== undefined) {
+          globalZIndex = +props.zIndex;
+        }
+
+        opened = true;
+        zIndex.value = ++globalZIndex;
+      }
+    };
+
+    const close = () => {
+      if (opened) {
+        opened = false;
+        emit('update:show', false);
+      }
+    };
+
+    const onClickOverlay = (event: MouseEvent) => {
+      emit('click-overlay', event);
+
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+
+    const onClickCloseIcon = (event: MouseEvent) => {
+      emit('click-close-icon', event);
+      close();
+    };
+
+    const onClick = (event: MouseEvent) => emit('click', event);
+    const onOpened = () => emit('opened');
+    const onClosed = () => emit('closed');
+
+    watch(
+      () => props.show,
+      (value) => {
+        if (value) {
+          open();
+          emit('open');
+        } else {
+          close();
+          emit('close');
+        }
+      }
+    );
+
+    useExpose({ popupRef });
+
+    useLockScroll(popupRef, () => props.show && props.lockScroll);
+
+    useEventListener('popstate', () => {
+      if (props.closeOnPopstate) {
+        close();
+        shouldReopen = false;
+      }
+    });
+
+    onMounted(() => {
+      if (props.show) {
+        open();
+      }
+    });
+
+    onActivated(() => {
+      if (shouldReopen) {
+        emit('update:show', true);
+        shouldReopen = false;
+      }
+    });
+
+    onDeactivated(() => {
+      if (props.show) {
+        close();
+        shouldReopen = true;
+      }
+    });
+
+    provide(POPUP_TOGGLE_KEY, () => props.show);
+
+    const name =
+      props.position === 'center'
+        ? 'c-fade'
+        : `c-popup-slide-${props.position}`;
+
+    return {
+      bem,
+      zIndex,
+      name,
+      popupRef,
+      style,
+      onClickOverlay,
+      onClick,
+      onOpened,
+      onClosed,
+      onClickCloseIcon,
+      attrs,
+    };
+  },
+};
+</script>
