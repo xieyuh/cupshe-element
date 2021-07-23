e7f1b822da0653815ccb6397d9f67f44e2f34b4a
xieyuheng <yuheng.xie@kapeixi.com>
2021-07-23 15:23:10 +0800

    feat(Swipe): add swipe coponent

---
+++ src/swipe-item/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div :class="bem()" :style="style">
+    <slot v-if="shouldRender" />
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, CSSProperties, nextTick, onMounted, reactive } from 'vue';
+import { createNamespace } from '../utils';
+import { useParent, useExpose } from '../composables';
+import { SWIPE_KEY } from '../swipe/index.vue';
+
+const [name, bem] = createNamespace('swipe-item');
+
+export default {
+  name,
+
+  setup() {
+    let rendered: boolean;
+
+    const state = reactive({
+      offset: 0,
+      inited: false,
+      mounted: false,
+    });
+
+    const { parent, index } = useParent(SWIPE_KEY);
+
+    const style = computed(() => {
+      const s: CSSProperties = {};
+      const { vertical } = parent.props;
+
+      if (parent.size.value) {
+        s[vertical ? 'height' : 'width'] = `${parent.size.value}px`;
+      }
+
+      if (state.offset) {
+        s.transform = `translate${vertical ? 'Y' : 'X'}(${state.offset}px)`;
+      }
+
+      return s;
+    });
+
+    const shouldRender = computed(() => {
+      const { loop, lazyRender } = parent.props;
+
+      if (!lazyRender || rendered) {
+        return true;
+      }
+
+      if (!state.mounted) {
+        return false;
+      }
+
+      const active = parent.activeIndicator.value;
+      const maxActive = parent.count.value - 1;
+      const prevActive = active === 0 && loop ? maxActive : active - 1;
+      const nextActive = active === maxActive && loop ? 0 : active + 1;
+      rendered =
+        index.value === active ||
+        index.value === prevActive ||
+        index.value === nextActive;
+
+      return rendered;
+    });
+
+    const setOffset = (offset: number) => {
+      state.offset = offset;
+    };
+
+    onMounted(() => {
+      nextTick(() => {
+        state.mounted = true;
+      });
+    });
+
+    useExpose({ setOffset });
+
+    return {
+      bem,
+      style,
+      shouldRender,
+    };
+  },
+};
+</script>
---
+++ src/swipe/demo/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <demo-block title="基础用法">
+    <c-swipe :autoplay="3000" indicator-color="white">
+      <c-swipe-item>1</c-swipe-item>
+      <c-swipe-item>2</c-swipe-item>
+      <c-swipe-item>3</c-swipe-item>
+      <c-swipe-item>4</c-swipe-item>
+    </c-swipe>
+  </demo-block>
+
+  <demo-block title="懒加载">
+    <c-swipe :autoplay="3000" lazy-render>
+      <c-swipe-item v-for="image in images" :key="image">
+        <img :src="image" />
+      </c-swipe-item>
+    </c-swipe>
+  </demo-block>
+
+  <demo-block title="监听 change 事件">
+    <c-swipe indicator-color="white" @change="onChange">
+      <c-swipe-item>1</c-swipe-item>
+      <c-swipe-item>2</c-swipe-item>
+      <c-swipe-item>3</c-swipe-item>
+      <c-swipe-item>4</c-swipe-item>
+    </c-swipe>
+  </demo-block>
+
+  <demo-block title="纵向滚动">
+    <c-swipe
+      vertical
+      :autoplay="3000"
+      indicator-color="white"
+      style="height: 200px"
+      class="demo-swipe--vertical"
+    >
+      <c-swipe-item>1</c-swipe-item>
+      <c-swipe-item>2</c-swipe-item>
+      <c-swipe-item>3</c-swipe-item>
+      <c-swipe-item>4</c-swipe-item>
+    </c-swipe>
+  </demo-block>
+
+  <demo-block title="自定义滑块大小">
+    <c-swipe :width="300" :loop="false" indicator-color="white">
+      <c-swipe-item>1</c-swipe-item>
+      <c-swipe-item>2</c-swipe-item>
+      <c-swipe-item>3</c-swipe-item>
+      <c-swipe-item>4</c-swipe-item>
+    </c-swipe>
+  </demo-block>
+
+  <demo-block title="自定义指示器">
+    <c-swipe>
+      <c-swipe-item>1</c-swipe-item>
+      <c-swipe-item>2</c-swipe-item>
+      <c-swipe-item>3</c-swipe-item>
+      <c-swipe-item>4</c-swipe-item>
+      <template #indicator="{ active }">
+        <div class="custom-indicator">{{ active + 1 }}/4</div>
+      </template>
+    </c-swipe>
+  </demo-block>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    const images = [
+      'https://img.yzcdn.cn/vant/apple-1.jpg',
+      'https://img.yzcdn.cn/vant/apple-2.jpg',
+      'https://img.yzcdn.cn/vant/apple-3.jpg',
+      'https://img.yzcdn.cn/vant/apple-4.jpg',
+    ];
+
+    const onChange = (index: number) =>
+      console.log('当前 Swipe 索引：' + index);
+
+    return {
+      images,
+      onChange,
+    };
+  },
+};
+</script>
+
+<style lang="less">
+@import '../../style/var';
+
+.demo-swipe {
+  padding-bottom: 30px;
+
+  .c-swipe {
+    &-item {
+      color: @white;
+      font-size: 20px;
+      line-height: 150px;
+      text-align: center;
+
+      &:nth-child(even) {
+        background-color: #39a9ed;
+      }
+
+      &:nth-child(odd) {
+        background-color: #66c6f2;
+      }
+    }
+
+    img {
+      display: block;
+      box-sizing: border-box;
+      width: 100%;
+      height: 240px;
+      padding: 30px 60px;
+      background-color: @white;
+      pointer-events: none;
+    }
+  }
+
+  &--vertical {
+    .c-swipe-item {
+      line-height: 200px;
+    }
+  }
+
+  .custom-indicator {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    padding: 2px 5px;
+    color: @white;
+    font-size: 12px;
+    background: rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
