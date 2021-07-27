acb95b636af93536fa22f6e5415d57cc7f395246
xieyuheng <yuheng.xie@kapeixi.com>
2021-07-27 09:16:08 +0800

    feat(Popover): add popover component

---
+++ src/popover/demo/index.vue
@@ -0,0 +1,204 @@
+<template>
+  <demo-block title="基础用法">
+    <c-popover
+      v-model:show="show.lightTheme"
+      :actions="[{ text: '选项一' }, { text: '选项二' }, { text: '选项三' }]"
+      placement="bottom-start"
+      @select="onSelect"
+    >
+      <template #reference>
+        <c-button type="primary">浅色风格</c-button>
+      </template>
+    </c-popover>
+    <c-popover
+      v-model:show="show.darkTheme"
+      theme="dark"
+      :actions="[{ text: '选项一' }, { text: '选项二' }, { text: '选项三' }]"
+      @select="onSelect"
+    >
+      <template #reference>
+        <c-button type="primary"> 深色风格 </c-button>
+      </template>
+    </c-popover>
+  </demo-block>
+
+  <!-- <demo-block title="弹出位置">
+    <c-field
+      is-link
+      readonly
+      name="picker"
+      label="选择弹出位置"
+      @click="showPicker = true"
+    />
+
+    <c-popup
+      v-model:show="showPicker"
+      round
+      position="bottom"
+      get-container="body"
+    >
+      <div class="demo-popover-box">
+        <c-popover
+          v-model:show="show.placement"
+          theme="dark"
+          :actions="[{ text: '选项一' }, { text: '选项二' }]"
+          :placement="currentPlacement"
+          @select="onSelect"
+        >
+          <template #reference>
+            <div class="demo-popover-refer" />
+          </template>
+        </c-popover>
+      </div>
+      <c-picker
+        :columns="placements"
+        :show-toolbar="false"
+        @change="onPickerChange"
+      />
+    </c-popup>
+  </demo-block> -->
+
+  <demo-block title="选项配置">
+    <c-popover
+      v-model:show="show.showIcon"
+      :actions="[
+        { text: '选项一', icon: 'plus' },
+        { text: '选项二', icon: 'tick' },
+        { text: '选项三', icon: 'search' },
+      ]"
+      placement="bottom-start"
+      @select="onSelect"
+    >
+      <template #reference>
+        <c-button type="primary"> 展示图标 </c-button>
+      </template>
+    </c-popover>
+
+    <c-popover
+      v-model:show="show.disableAction"
+      :actions="[
+        { text: '选项一', disabled: true },
+        { text: '选项二', disabled: true },
+        { text: '选项三' },
+      ]"
+      @select="onSelect"
+    >
+      <template #reference>
+        <c-button type="primary"> 禁用选项 </c-button>
+      </template>
+    </c-popover>
+  </demo-block>
+
+  <!-- <demo-block title="自定义内容">
+    <c-popover
+      v-model:show="show.customContent"
+      placement="top-start"
+      @select="onSelect"
+    >
+      <c-grid
+        square
+        clickable
+        :border="false"
+        column-num="3"
+        style="width: 240px"
+      >
+        <c-grid-item
+          v-for="i in 6"
+          :key="i"
+          icon="photo-o"
+          text="选项"
+          @click="show.customContent = false"
+        />
+      </c-grid>
+      <template #reference>
+        <c-button type="primary"> 自定义内容 </c-button>
+      </template>
+    </c-popover>
+  </demo-block> -->
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue';
+
+const placements = [
+  'top',
+  'top-start',
+  'top-end',
+  'left',
+  'left-start',
+  'left-end',
+  'right',
+  'right-start',
+  'right-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+];
+
+export default {
+  setup() {
+    const state = reactive({
+      show: {
+        showIcon: false,
+        placement: false,
+        darkTheme: false,
+        lightTheme: false,
+        customContent: false,
+        disableAction: false,
+      },
+      showPicker: false,
+      currentPlacement: 'top',
+    });
+
+    const onPickerChange = (value: string) => {
+      setTimeout(() => {
+        state.show.placement = true;
+        state.currentPlacement = value;
+      });
+    };
+
+    const onSelect = (action: { text: string }) => console.log(action.text);
+
+    return {
+      ...toRefs(state),
+      onSelect,
+      placements,
+      onPickerChange,
+    };
+  },
+};
+</script>
+
+<style lang="less">
+@import '../../style/var';
+
+.demo-popover {
+  &-refer {
+    width: 60px;
+    height: 60px;
+    background-color: @blue;
+    border-radius: 8px;
+  }
+
+  .c-popover__wrapper {
+    margin-left: @padding-md;
+  }
+
+  .c-field {
+    width: auto;
+    margin: 0 12px;
+    overflow: hidden;
+    border-radius: 8px;
+  }
+
+  &-box {
+    display: flex;
+    justify-content: center;
+    margin: 110px 0;
+
+    .c-popover__wrapper {
+      margin-left: 0;
+    }
+  }
+}
+</style>
---
+++ src/popover/index.vue
@@ -0,0 +1,247 @@
+<template>
+  <span ref="wrapperRef" :class="bem('wrapper')" @click="onClickWrapper">
+    <slot name="reference" />
+  </span>
+  <c-popup
+    ref="popoverRef"
+    :class="bem([theme])"
+    :lock-scroll="false"
+    position=""
+    transition="c-popover-zoom"
+    @touchstart="onTouchstart"
+    v-bind="attrs"
+    :show="show"
+    @update:show="updateShow"
+  >
+    <div :class="bem('arrow')" />
+    <div role="menu" :class="bem('content')">
+      <slot>
+        <div
+          role="menuitem"
+          :class="[
+            bem('action', {
+              disabled: item.disabled,
+              'with-icon': item.icon,
+            }),
+            item.className,
+          ]"
+          :style="{ color: item.color }"
+          v-for="(item, index) in actions"
+          :key="index"
+          @click="onClickAction(item, index)"
+        >
+          <c-icon
+            v-if="item.icon"
+            :name="item.icon"
+            :class="bem('action-icon')"
+          />
+          <div :class="[bem('action-text'), BORDER_BOTTOM]">
+            {{ item.text }}
+          </div>
+        </div>
+      </slot>
+    </div>
+  </c-popup>
+</template>
+
+<script lang="ts">
+import {
+  nextTick,
+  onMounted,
+  onBeforeUnmount,
+  PropType,
+  TeleportProps,
+  CSSProperties,
+  ref,
+  watch,
+} from 'vue';
+import { Instance, createPopper, offsetModifier } from '../utils/popper';
+import {
+  createNamespace,
+  truthProp,
+  unknownProp,
+  ComponentInstance,
+  extend,
+  pick,
+} from '../utils';
+import { useClickAway } from '../composables';
+import { BORDER_BOTTOM } from '../utils/constant';
+
+const [name, bem] = createNamespace('popover');
+
+const popupProps = [
+  'overlay',
+  'duration',
+  'teleport',
+  'overlayStyle',
+  'overlayClass',
+  'closeOnClickOverlay',
+] as const;
+
+export type PopoverTheme = 'light' | 'dark';
+export type PopoverTrigger = 'manual' | 'click';
+export type PopoverPlacement =
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end';
+
+export type PopoverAction = {
+  text: string;
+  icon?: string;
+  color?: string;
+  disabled?: boolean;
+  className?: string;
+};
+
+export default {
+  name,
+
+  props: {
+    show: Boolean,
+    overlay: Boolean,
+    duration: [Number, String],
+    iconPrefix: String,
+    overlayClass: unknownProp,
+    overlayStyle: Object as PropType<CSSProperties>,
+    closeOnClickAction: truthProp,
+    closeOnClickOverlay: truthProp,
+    closeOnClickOutside: truthProp,
+    offset: {
+      type: (Array as unknown) as PropType<[number, number]>,
+      default: () => [0, 8],
+    },
+    theme: {
+      type: String as PropType<PopoverTheme>,
+      default: 'light',
+    },
+    trigger: {
+      type: String as PropType<PopoverTrigger>,
+      default: 'click',
+    },
+    actions: {
+      type: Array as PropType<PopoverAction[]>,
+      default: () => [],
+    },
+    placement: {
+      type: String as PropType<PopoverPlacement>,
+      default: 'bottom',
+    },
+    teleport: {
+      type: [String, Object] as PropType<TeleportProps['to']>,
+      default: 'body',
+    },
+  },
+
+  emits: ['select', 'touchstart', 'update:show'],
+
+  setup(props, { emit, attrs }) {
+    let popper: Instance | null;
+
+    const wrapperRef = ref<HTMLElement>();
+    const popoverRef = ref<ComponentInstance>();
+
+    const createPopperInstance = () => {
+      return createPopper(wrapperRef.value!, popoverRef.value!.popupRef, {
+        placement: props.placement,
+        modifiers: [
+          {
+            name: 'computeStyles',
+            options: {
+              adaptive: false,
+              gpuAcceleration: false,
+            },
+          },
+          extend({}, offsetModifier, {
+            options: {
+              offset: props.offset,
+            },
+          }),
+        ],
+      });
+    };
+
+    const updateLocation = () => {
+      nextTick(() => {
+        if (!props.show) {
+          return;
+        }
+
+        if (!popper) {
+          popper = createPopperInstance();
+        } else {
+          popper.setOptions({
+            placement: props.placement,
+          });
+        }
+      });
+    };
+
+    const updateShow = (value: boolean) => emit('update:show', value);
+
+    const onClickWrapper = () => {
+      if (props.trigger === 'click') {
+        updateShow(!props.show);
+      }
+    };
+
+    const onTouchstart = (event: TouchEvent) => {
+      event.stopPropagation();
+      emit('touchstart', event);
+    };
+
+    const onClickAction = (action: PopoverAction, index: number) => {
+      if (action.disabled) {
+        return;
+      }
+
+      emit('select', action, index);
+
+      if (props.closeOnClickAction) {
+        updateShow(false);
+      }
+    };
+
+    const onClickAway = () => {
+      if (
+        props.closeOnClickOutside &&
+        (!props.overlay || props.closeOnClickOverlay)
+      ) {
+        updateShow(false);
+      }
+    };
+
+    onMounted(updateLocation);
+    onBeforeUnmount(() => {
+      if (popper) {
+        popper.destroy();
+        popper = null;
+      }
+    });
+
+    watch(() => [props.show, props.placement], updateLocation);
+
+    useClickAway(wrapperRef, onClickAway, { eventName: 'touchstart' });
+
+    return {
+      bem,
+      wrapperRef,
+      popoverRef,
+      onClickWrapper,
+      onTouchstart,
+      updateShow,
+      attrs: { ...attrs, ...pick(props, popupProps) },
+      onClickAction,
+      BORDER_BOTTOM,
+    };
+  },
+};
+</script>
