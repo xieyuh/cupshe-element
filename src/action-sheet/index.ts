fc6e7fadfd8d6137c394ec7a630d7c0a5bb4c4cd
xieyuheng <yuheng.xie@kapeixi.com>
2022-03-05 12:42:25 +0800

    fix(ActionSheet): fix export

--- src/action-sheet/index.ts
+++ src/action-sheet/index.ts
@@ -1,8 +1,9 @@
 import { withInstall } from '../utils';
 import _ActionSheet from './ActionSheet';
 
-export const ActionSheet = withInstall(_ActionSheet);
+const ActionSheet = withInstall(_ActionSheet);
 export default ActionSheet;
+export { ActionSheet };
 export type { ActionSheetProps, ActionSheetAction } from './ActionSheet';
 
 declare module 'vue' {
