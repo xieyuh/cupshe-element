b6c3a7b2745e8133744e8a591463aa80dd650810
xieyuheng <yuheng.xie@kapeixi.com>
2022-03-14 17:36:31 +0800

    docs(Icon): update icon docs

--- src/icon/demo/index.vue
+++ src/icon/demo/index.vue
@@ -1,50 +1,16 @@
 <template>
   <demo-block title="基础图标">
     <c-row>
-      <c-col span="6">
-        <c-icon name="chat" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="tick" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="share_facebook" />
-      </c-col>
-    </c-row>
-  </demo-block>
-
-  <demo-block title="图标颜色">
-    <c-row>
-      <c-col span="6">
-        <c-icon name="star" color="#1989fa" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="share_twitter" color="#ee0a24" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="sale_full" color="#07c160" />
-      </c-col>
-    </c-row>
-  </demo-block>
-
-  <demo-block title="图标大小">
-    <c-row>
-      <c-col span="6">
-        <c-icon name="email" size="24" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="info" size="2.5rem" />
-      </c-col>
-      <c-col span="6">
-        <c-icon name="like_full" size="3rem" />
+      <c-col v-for="icon in icons" :key="icon" span="6" @click="copy(icon)">
+        <c-icon :name="icon" />
+        <span>{{ icon }}</span>
       </c-col>
     </c-row>
   </demo-block>
 </template>
 
-<script lang="ts">
-import { ref } from 'vue';
-
+<script lang="ts" setup>
+import { Toast } from '../../toast';
 // from https://30secondsofcode.org
 function copyToClipboard(str: string) {
   const el = document.createElement('textarea');
@@ -72,42 +38,152 @@ function copyToClipboard(str: string) {
   }
 }
 
-export default {
-  setup() {
-    const tab = ref(0);
+const icons = [
+  'arrow_down',
+  'arrow_up',
+  'arrow_left',
+  'arrow_right',
+  'close',
+  'plus',
+  'tick',
+  'search',
+  'wishlist_nor',
+  'account',
+  'bag',
+  'dislike',
+  'like',
+  'safe',
+  'shippinginfo',
+  'sizefit',
+  'sizeguide',
+  'package',
+  'info',
+  'delete',
+  'copy',
+  'iphone',
+  'edit',
+  'chat',
+  'email',
+  'ticket',
+  'share',
+  'update',
+  'giftcard',
+  'sale',
+  'sort',
+  'filter',
+  'points',
+  'time',
+  'video',
+  'warning',
+  'question',
+  'success',
+  'failure',
+  'right',
+  'left',
+  'minus',
+  'menu',
+  'play',
+  'pause',
+  'star',
+  'country',
+  'update_delete',
+  'video_full',
+  'account_sel',
+  'down_full',
+  'account2_full',
+  'delivery_full',
+  'other_full',
+  'package_full',
+  'payment_full',
+  'safe_full',
+  'update_full',
+  'size_full',
+  'sunchasers_full',
+  'ticket_full',
+  'returns_full',
+  'giftcard_full',
+  'sale_full',
+  'addaddress',
+  'success_full',
+  'left_full',
+  'right_full',
+  'question_full',
+  'info_full',
+  'star_full',
+  'star_half',
+  'dislike_full',
+  'like_full',
+  'icon_shop',
+  'nextstep',
+  'share_facebook',
+  'share_linkin',
+  'share_pinterest',
+  'share_instagram',
+  'share_tiktok',
+  'share_twitter',
+  'share_messenger',
+  'share_youtube',
+  'share_snapchat',
+  'share_whats',
+  'share_mail',
+  'return_documents',
+  'contact',
+  'return_successde',
+  'return_successau',
+  'return_instructions',
+  'createaccount1',
+  'afterpay',
+  'anniversary',
+  'birthday',
+  'checkout',
+  'facebook',
+  'honorarymember',
+  'instagram',
+  'exclusivediscounts',
+  'personalexposure',
+  'product_4',
+  'makeapurchase_au',
+  'freeshipping_fast',
+  'makeapurchase_de',
+  'product_2',
+  'product_6',
+  'product_1',
+  'klarna',
+  'freeshipping',
+  'product_7',
+  'refer',
+  'product_3',
+  'redeem_coupon',
+  'signin_monthly',
+  'tierup',
+  'product_5',
+  'sale_early',
+  'voucher_freeproduct',
+  'subscribe',
+  'surprisepackage',
+  'zahlen',
+  'valued_coupon',
+  'writeareview',
+  'afterpay_1',
+  'afterpay_4',
+  'afterpay_2',
+  'afterpay_3',
+  'checkbox_nor',
+  'radio_sel',
+  'checkbox_sel',
+  'radio_nor',
+];
 
-    const copy = (icon: string, option: Record<string, unknown> = {}) => {
-      let tag = `<c-icon name="${icon}"`;
-      if ('dot' in option) {
-        tag = `${tag} ${option.dot ? 'dot' : ''}`;
-      }
-      if ('badge' in option) {
-        tag = `${tag} badge="${option.badge}"`;
-      }
-      if ('color' in option) {
-        tag = `${tag} color="${option.color}"`;
-      }
-      if ('size' in option) {
-        tag = `${tag} size="${option.size}"`;
-      }
-      tag = `${tag} />`;
-      copyToClipboard(tag);
+const copy = (icon: string) => {
+  const tag = `<c-icon name="${icon}"`;
+  copyToClipboard(tag);
 
-      // Notify({
-      //   type: 'success',
-      //   duration: 1500,
-      //   className: 'demo-icon-notify',
-      //   message: `${t('copied')}：${tag}`,
-      // });
-    };
-
-    return {
-      tab,
-      copy,
-      demoIcon: 'chat-o',
-      demoImage: 'https://b.yzcdn.cn/vant/icon-demo-1126.png',
-    };
-  },
+  Toast({
+    type: 'success',
+    duration: 1500,
+    className: 'demo-icon-notify',
+    message: '已复制',
+  });
 };
 </script>
 
